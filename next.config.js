1eaadcae0ac29482f06d6b8896e2cbf7f7130bed
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-14 18:26:26 +0530

    change the basePath

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 const nextConfig = {
   output:  process.env.NODE_ENV !== "production" ? undefined: "export",
+  basePath: '/deploy-nextjs-app-router-github-pages',
   images: {
     unoptimized: envImageUnoptimize,
     remotePatterns: [
