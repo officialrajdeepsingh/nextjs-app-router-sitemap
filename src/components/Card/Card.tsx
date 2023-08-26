aa38f919d749dfa912ca70e43e4b50b2a428f3f3
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-26 13:36:04 +0530

    update the UI

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,6 @@
 import { Badge, Card, Flex, Heading,Container,Button } from "@radix-ui/themes";
 import Link from "next/link";
+
 interface Post {
   id: number;
   title: string;
@@ -10,8 +11,8 @@ interface Post {
 }
 export function BlogCard({ post }: { post: Post }) {
   return (
-    <Container p={"5"} my={"9"}>
-      <Card my={"9"} size="3">
+    <Container p={"6"} my={"9"}>
+      <Card my={"9"} size="5">
         <Flex
           gap={"5"}
           align={"start"}
@@ -19,19 +20,23 @@ export function BlogCard({ post }: { post: Post }) {
           justify={"center"}
           direction={"column"}
         >
+          
           <Heading as="h2" color="gray" mb="1" size="5">
             {post.title}
           </Heading>
+
           <Flex gap="2">
             {post.tags.map(
               (tag) => <Badge size={"2"} key={tag}>{tag}</Badge>,
             )}
           </Flex>
+          
           <Link href={`/read/${post.id}`}>
             <Button size={"3"}>
                 Read More
             </Button>
           </Link>
+        
         </Flex>
       </Card>
     </Container>
